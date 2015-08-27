4512fc15db342c9d9574921f6d31f21751d44b86
OsamaRasheed <osamarasheedofficial@gmail.com>
2015-08-27 23:06:50 +0500

    final changes adding plan created field

--- plan_marker/urls.py
+++ plan_marker/urls.py
@@ -4,5 +4,6 @@ urlpatterns = [
     url(r'^login/$', 'plan_marker.views.login'),
     url(r'^signup/$', 'plan_marker.views.signup'),
     url(r'^excercise/$', 'plan_marker.views.excercise_handler'),
+    url(r'^plan_created/$', 'plan_marker.views.plan_handler'),
 
 ]
