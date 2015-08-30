caccead7e99554fb625c4068562b4613dc5d79db
Ubuntu <ubuntu@ip-172-31-46-27.us-west-2.compute.internal>
2015-08-30 20:48:42 +0000

    changes

--- templates/lib/js/settings.js
+++ templates/lib/js/settings.js
@@ -1,1 +1,1 @@
-function Host(a,b){return host=a.toString()+":"+b.toString()}var host="";Host("localhost","8000");
+function Host(a,b){return host=a.toString()+":"+b.toString()}var host="";Host("52.26.185.73","8000");
